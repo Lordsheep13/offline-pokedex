f8bba78bc43998102eea8fd7c60de2d6f6a4a208
Lordsheep13 <kinggoat343@gmail.com>
2025-08-17 17:24:50 +0100

    created window

--- pokedex.c
+++ pokedex.c
@@ -1,11 +1,29 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <string.h>
-#include <gtk/gtk.h>
-#include <cjson/cJSON.h>
+//#include <cjson/cJSON.h>
 
-int main(){
+static void activate (GtkApplication* app, gpointer user_data);
 
-	printf("hello");
+int main (int argc, char **argv){
 
-	return 0;
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
+
+static void activate (GtkApplication* app, gpointer user_data){
+
+	GtkWidget *window;
+
+	window = gtk_application_window_new (app);
+  	gtk_window_set_title (GTK_WINDOW (window), "Offline Pokedex");
+  	gtk_window_set_default_size (GTK_WINDOW (window), 600, 400);
+  	gtk_widget_show_all (window);
 }
